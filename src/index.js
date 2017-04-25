10e807f8562bf177ae1616134a9cadd9a6f17212
arindampradhan <arindampradhan10@gmail.com>
2017-04-25 17:47:56 +0530

    react simple pack

---
+++ src/index.js
@@ -0,0 +1,546 @@
+import React, { Component } from 'react'
+import Calendar from './components/Calendar'
+import VolumeOff from './components/VolumeOff'
+import MinusO from './components/MinusO'
+import BagItemRemove from './components/BagItemRemove'
+import AddO from './components/AddO'
+import YoutubeSquare from './components/YoutubeSquare'
+import StarO from './components/StarO'
+import Youtube from './components/Youtube'
+import Download from './components/Download'
+import CloudSnowO from './components/CloudSnowO'
+import DriveO from './components/DriveO'
+import HeartO from './components/HeartO'
+import Reply from './components/Reply'
+import Instagram from './components/Instagram'
+import Basketball from './components/Basketball'
+import Network from './components/Network'
+import CloneTrooper from './components/CloneTrooper'
+import CardPocketOpen from './components/CardPocketOpen'
+import ListOrdered from './components/ListOrdered'
+import Bin from './components/Bin'
+import CloseO from './components/CloseO'
+import Star from './components/Star'
+import FacebookO from './components/FacebookO'
+import BarChart from './components/BarChart'
+import Basket from './components/Basket'
+import CashPack from './components/CashPack'
+import Dropbox from './components/Dropbox'
+import MoonWaningCrescent from './components/MoonWaningCrescent'
+import ParagraphLeft from './components/ParagraphLeft'
+import BagItemPut from './components/BagItemPut'
+import MobileO from './components/MobileO'
+import Pin from './components/Pin'
+import Reverse from './components/Reverse'
+import MailRect from './components/MailRect'
+import Picture from './components/Picture'
+import ParagraphRight from './components/ParagraphRight'
+import CashPackO from './components/CashPackO'
+import Cart from './components/Cart'
+import VolumeOn from './components/VolumeOn'
+import AccountName from './components/AccountName'
+import MoonWaxingGibbous from './components/MoonWaxingGibbous'
+import Book from './components/Book'
+import Mic from './components/Mic'
+import Paragraph from './components/Paragraph'
+import TemperatureCelsius from './components/TemperatureCelsius'
+import CheckoutItemAdd from './components/CheckoutItemAdd'
+import GooglePlus from './components/GooglePlus'
+import Plus from './components/Plus'
+import Drawer from './components/Drawer'
+import File from './components/File'
+import Bell from './components/Bell'
+import TwitterO2 from './components/TwitterO2'
+import SolarSystem from './components/SolarSystem'
+import Square from './components/Square'
+import Music from './components/Music'
+import Marker from './components/Marker'
+import RedoO from './components/RedoO'
+import Heart from './components/Heart'
+import Dustbin from './components/Dustbin'
+import CardPayment from './components/CardPayment'
+import Name658 from './components/Name658'
+import CloudThunder from './components/CloudThunder'
+import CashPound from './components/CashPound'
+import LinkUrlO from './components/LinkUrlO'
+import Navigation from './components/Navigation'
+import SettingsCog from './components/SettingsCog'
+import GoogleO from './components/GoogleO'
+import BagItemAdd from './components/BagItemAdd'
+import VimeoSquare from './components/VimeoSquare'
+import MoonWaxingCrescent from './components/MoonWaxingCrescent'
+import LeftLong from './components/LeftLong'
+import VideoFilm from './components/VideoFilm'
+import WindowTab from './components/WindowTab'
+import Link from './components/Link'
+import Draw from './components/Draw'
+import Drive from './components/Drive'
+import SoundUp from './components/SoundUp'
+import ComputerMac from './components/ComputerMac'
+import CheckoutItem from './components/CheckoutItem'
+import Forward from './components/Forward'
+import CameraSnap from './components/CameraSnap'
+import Account from './components/Account'
+import BatteryFull from './components/BatteryFull'
+import SettingsO from './components/SettingsO'
+import Money from './components/Money'
+import MoonThirdQuarter from './components/MoonThirdQuarter'
+import InstgramO from './components/InstgramO'
+import CameraOpen from './components/CameraOpen'
+import CashMultiple from './components/CashMultiple'
+import Cash from './components/Cash'
+import SoundOn from './components/SoundOn'
+import YoutubeO from './components/YoutubeO'
+import Wine from './components/Wine'
+import Pendulum from './components/Pendulum'
+import Air from './components/Air'
+import MoonFull from './components/MoonFull'
+import WifiO from './components/WifiO'
+import Phone from './components/Phone'
+import Tablet from './components/Tablet'
+import Charger from './components/Charger'
+import Toggle from './components/Toggle'
+import BasketballO from './components/BasketballO'
+import Delete from './components/Delete'
+import BatteryZero from './components/BatteryZero'
+import MapO from './components/MapO'
+import List from './components/List'
+import SunSet from './components/SunSet'
+import MailSquare from './components/MailSquare'
+import FolderOpen from './components/FolderOpen'
+import Thermometer from './components/Thermometer'
+import Wrong from './components/Wrong'
+import Vimeo from './components/Vimeo'
+import FacebookSquare from './components/FacebookSquare'
+import SearchO from './components/SearchO'
+import LocationPointer from './components/LocationPointer'
+import Info from './components/Info'
+import TemperatureFahrenheit from './components/TemperatureFahrenheit'
+import LinkUrl from './components/LinkUrl'
+import TickO from './components/TickO'
+import PowerLow from './components/PowerLow'
+import Billboard from './components/Billboard'
+import CrossO from './components/CrossO'
+import Down from './components/Down'
+import Icons from './components/Icons'
+import CalendarO from './components/CalendarO'
+import Drop from './components/Drop'
+import Share from './components/Share'
+import Briefcase from './components/Briefcase'
+import PieChart from './components/PieChart'
+import MailO2 from './components/MailO2'
+import MessageFill from './components/MessageFill'
+import MusicO from './components/MusicO'
+import CloudCumulus from './components/CloudCumulus'
+import Bucket from './components/Bucket'
+import SunRise from './components/SunRise'
+import LocationO from './components/LocationO'
+import ChatList from './components/ChatList'
+import BriefcaseOO from './components/BriefcaseOO'
+import GoogleSquare from './components/GoogleSquare'
+import CreditCard from './components/CreditCard'
+import DropboxSquare from './components/DropboxSquare'
+import Lest from './components/Lest'
+import Umberella from './components/Umberella'
+import Paper from './components/Paper'
+import TwitterO from './components/TwitterO'
+import CardsPayment from './components/CardsPayment'
+import MapPoint from './components/MapPoint'
+import Clock from './components/Clock'
+import FileEmpty from './components/FileEmpty'
+import Tower from './components/Tower'
+import Eye from './components/Eye'
+import CloudSnow from './components/CloudSnow'
+import Location from './components/Location'
+import MailO from './components/MailO'
+import HomeO from './components/HomeO'
+import Pintrest from './components/Pintrest'
+import Minus from './components/Minus'
+import Sun from './components/Sun'
+import Quote from './components/Quote'
+import CircularChart from './components/CircularChart'
+import Focus from './components/Focus'
+import Tag from './components/Tag'
+import Papers from './components/Papers'
+import BinO from './components/BinO'
+import WindowTabs from './components/WindowTabs'
+import CardPocket from './components/CardPocket'
+import FrameToggle from './components/FrameToggle'
+import Copy from './components/Copy'
+import SoundOff from './components/SoundOff'
+import Name398 from './components/Name398'
+import ToggleO from './components/ToggleO'
+import Upload from './components/Upload'
+import Name536 from './components/Name536'
+import PinterestO from './components/PinterestO'
+import Up from './components/Up'
+import DownloadO from './components/DownloadO'
+import RightO from './components/RightO'
+import Files from './components/Files'
+import ParagraphCenter from './components/ParagraphCenter'
+import Check from './components/Check'
+import CloudO from './components/CloudO'
+import Twitter from './components/Twitter'
+import ChartUp from './components/ChartUp'
+import Cloud from './components/Cloud'
+import Name430 from './components/Name430'
+import Search from './components/Search'
+import DarthVader from './components/DarthVader'
+import Folder from './components/Folder'
+import Correct from './components/Correct'
+import MessageSending from './components/MessageSending'
+import Name968 from './components/Name968'
+import CameraO from './components/CameraO'
+import FileOpen from './components/FileOpen'
+import VimeoO from './components/VimeoO'
+import Chat from './components/Chat'
+import Saturn from './components/Saturn'
+import CashO from './components/CashO'
+import R2D2 from './components/R2D2'
+import BatteryOne from './components/BatteryOne'
+import MoonWaningGibbous from './components/MoonWaningGibbous'
+import Right from './components/Right'
+import RightLong from './components/RightLong'
+import Power from './components/Power'
+import AccountO from './components/AccountO'
+import CloudCloudy from './components/CloudCloudy'
+import Left from './components/Left'
+import MoonNew from './components/MoonNew'
+import Map from './components/Map'
+import Print from './components/Print'
+import Crop from './components/Crop'
+import WrongO from './components/WrongO'
+import CashMultipleO from './components/CashMultipleO'
+import ClockO from './components/ClockO'
+import Save from './components/Save'
+import Backward from './components/Backward'
+import Computer from './components/Computer'
+import Settings from './components/Settings'
+import LeiaOrgana from './components/LeiaOrgana'
+import ChatConversation from './components/ChatConversation'
+import EyeOpen from './components/EyeOpen'
+import Home from './components/Home'
+import Bag from './components/Bag'
+import TwitterT from './components/TwitterT'
+import StarHanging from './components/StarHanging'
+import TwitterO2Square from './components/TwitterO2Square'
+import Video from './components/Video'
+import InstagramSquare from './components/InstagramSquare'
+import ChatAvailable from './components/ChatAvailable'
+import Camera from './components/Camera'
+import Frame from './components/Frame'
+import MoneyO from './components/MoneyO'
+import SoundDown from './components/SoundDown'
+import Timer from './components/Timer'
+import Redo from './components/Redo'
+import CloudStorm from './components/CloudStorm'
+import SettingsCogs from './components/SettingsCogs'
+import Rainbow from './components/Rainbow'
+import BasketballSquare from './components/BasketballSquare'
+import Screens from './components/Screens'
+import Facebook from './components/Facebook'
+import PinterestSquare from './components/PinterestSquare'
+import Lock from './components/Lock'
+import CreditCardO from './components/CreditCardO'
+import LeftO from './components/LeftO'
+import DropboxO from './components/DropboxO'
+import HeartBeat from './components/HeartBeat'
+import MoonFirstQuarter from './components/MoonFirstQuarter'
+import Add from './components/Add'
+import ListO2 from './components/ListO2'
+import DrawO from './components/DrawO'
+import AccountCircle from './components/AccountCircle'
+import Message from './components/Message'
+import BriefcaseO from './components/BriefcaseO'
+import Printer from './components/Printer'
+import Accounts from './components/Accounts'
+import CheckoutItemRemove from './components/CheckoutItemRemove'
+import Pencil from './components/Pencil'
+import Bookmark from './components/Bookmark'
+import Console from './components/Console'
+import RefreshO from './components/RefreshO'
+import Moon from './components/Moon'
+import Name540 from './components/Name540'
+import TwitterSquare from './components/TwitterSquare'
+import Close from './components/Close'
+import C3Po from './components/C3Po'
+import CloudRain from './components/CloudRain'
+import Name696 from './components/Name696'
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <Calendar />
+        <MinusO />
+        <BagItemRemove />
+        <AddO />
+        <YoutubeSquare />
+        <StarO />
+        <Youtube />
+        <Download />
+        <CloudSnowO />
+        <DriveO />
+        <HeartO />
+        <Reply />
+        <VolumeOff />
+        <Instagram />
+        <Basketball />
+        <Network />
+        <CloneTrooper />
+        <CardPocketOpen />
+        <ListOrdered />
+        <Bin />
+        <CloseO />
+        <Star />
+        <FacebookO />
+        <BarChart />
+        <Basket />
+        <CashPack />
+        <Dropbox />
+        <MoonWaningCrescent />
+        <ParagraphLeft />
+        <BagItemPut />
+        <MobileO />
+        <Pin />
+        <Reverse />
+        <MailRect />
+        <Picture />
+        <ParagraphRight />
+        <CashPackO />
+        <Cart />
+        <VolumeOn />
+        <AccountName />
+        <MoonWaxingGibbous />
+        <Book />
+        <Mic />
+        <Paragraph />
+        <TemperatureCelsius />
+        <CheckoutItemAdd />
+        <GooglePlus />
+        <Plus />
+        <Drawer />
+        <File />
+        <Bell />
+        <TwitterO2 />
+        <SolarSystem />
+        <Square />
+        <Music />
+        <Marker />
+        <RedoO />
+        <Heart />
+        <Dustbin />
+        <CardPayment />
+        <Name658 />
+        <CloudThunder />
+        <CashPound />
+        <LinkUrlO />
+        <Navigation />
+        <SettingsCog />
+        <GoogleO />
+        <BagItemAdd />
+        <VimeoSquare />
+        <MoonWaxingCrescent />
+        <LeftLong />
+        <VideoFilm />
+        <WindowTab />
+        <Link />
+        <Draw />
+        <Drive />
+        <SoundUp />
+        <ComputerMac />
+        <CheckoutItem />
+        <Forward />
+        <CameraSnap />
+        <Account />
+        <BatteryFull />
+        <SettingsO />
+        <Money />
+        <MoonThirdQuarter />
+        <InstgramO />
+        <CameraOpen />
+        <CashMultiple />
+        <Cash />
+        <SoundOn />
+        <YoutubeO />
+        <Wine />
+        <Pendulum />
+        <Air />
+        <MoonFull />
+        <WifiO />
+        <Phone />
+        <Tablet />
+        <Charger />
+        <Toggle />
+        <BasketballO />
+        <Delete />
+        <BatteryZero />
+        <MapO />
+        <List />
+        <SunSet />
+        <MailSquare />
+        <FolderOpen />
+        <Thermometer />
+        <Wrong />
+        <Vimeo />
+        <FacebookSquare />
+        <SearchO />
+        <LocationPointer />
+        <Info />
+        <TemperatureFahrenheit />
+        <LinkUrl />
+        <TickO />
+        <PowerLow />
+        <Billboard />
+        <CrossO />
+        <Down />
+        <Icons />
+        <CalendarO />
+        <Drop />
+        <Share />
+        <Briefcase />
+        <PieChart />
+        <MailO2 />
+        <MessageFill />
+        <MusicO />
+        <CloudCumulus />
+        <Bucket />
+        <SunRise />
+        <LocationO />
+        <ChatList />
+        <BriefcaseOO />
+        <GoogleSquare />
+        <CreditCard />
+        <DropboxSquare />
+        <Lest />
+        <Umberella />
+        <Paper />
+        <TwitterO />
+        <CardsPayment />
+        <MapPoint />
+        <Clock />
+        <FileEmpty />
+        <Tower />
+        <Eye />
+        <CloudSnow />
+        <Location />
+        <MailO />
+        <HomeO />
+        <Pintrest />
+        <Minus />
+        <Sun />
+        <Quote />
+        <CircularChart />
+        <Focus />
+        <Tag />
+        <Papers />
+        <BinO />
+        <WindowTabs />
+        <CardPocket />
+        <FrameToggle />
+        <Copy />
+        <SoundOff />
+        <Name398 />
+        <ToggleO />
+        <Upload />
+        <Name536 />
+        <PinterestO />
+        <Up />
+        <DownloadO />
+        <RightO />
+        <Files />
+        <ParagraphCenter />
+        <Check />
+        <CloudO />
+        <Twitter />
+        <ChartUp />
+        <Cloud />
+        <Name430 />
+        <Search />
+        <DarthVader />
+        <Folder />
+        <Correct />
+        <MessageSending />
+        <Name968 />
+        <CameraO />
+        <FileOpen />
+        <VimeoO />
+        <Chat />
+        <Saturn />
+        <CashO />
+        <R2D2 />
+        <BatteryOne />
+        <MoonWaningGibbous />
+        <Right />
+        <RightLong />
+        <Power />
+        <AccountO />
+        <CloudCloudy />
+        <Left />
+        <MoonNew />
+        <Map />
+        <Print />
+        <Crop />
+        <WrongO />
+        <CashMultipleO />
+        <ClockO />
+        <Save />
+        <Backward />
+        <Computer />
+        <Settings />
+        <LeiaOrgana />
+        <ChatConversation />
+        <EyeOpen />
+        <Home />
+        <Bag />
+        <TwitterT />
+        <StarHanging />
+        <TwitterO2Square />
+        <Video />
+        <InstagramSquare />
+        <ChatAvailable />
+        <Camera />
+        <Frame />
+        <MoneyO />
+        <SoundDown />
+        <Timer />
+        <Redo />
+        <CloudStorm />
+        <SettingsCogs />
+        <Rainbow />
+        <BasketballSquare />
+        <Screens />
+        <Facebook />
+        <PinterestSquare />
+        <Lock />
+        <CreditCardO />
+        <LeftO />
+        <DropboxO />
+        <HeartBeat />
+        <MoonFirstQuarter />
+        <Add />
+        <ListO2 />
+        <DrawO />
+        <AccountCircle />
+        <Message />
+        <BriefcaseO />
+        <Printer />
+        <Accounts />
+        <CheckoutItemRemove />
+        <Pencil />
+        <Bookmark />
+        <Console />
+        <RefreshO />
+        <Moon />
+        <Name540 />
+        <TwitterSquare />
+        <Close />
+        <C3Po />
+        <CloudRain />
+        <Name696 />
+      </div>
+    )
+  }
+}
+
+export default App
